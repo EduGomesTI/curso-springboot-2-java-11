eecbd4060fb85f48f82022a526ac9e732db81768
EduGomeTI <edugomesti@outlook.com>
2021-02-27 20:26:53 -0300

    User entity and resource

---
+++ src/main/java/com/cursojava/workshop/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.cursojava.workshop.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cursojava.workshop.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Eduardo", "edu@gmil.com", "99999999", "123435");
+		return ResponseEntity.ok().body(u);		
+	}
+	
+}
