9c30166535f69af3c0a4710e37deafe79d7d9bc3
EduGomeTI <edugomesti@outlook.com>
2021-03-01 14:54:03 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/com/cursojava/workshop/config/TestConfig.java
+++ src/main/java/com/cursojava/workshop/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.cursojava.workshop.entities.Category;
 import com.cursojava.workshop.entities.Order;
+import com.cursojava.workshop.entities.OrderItem;
 import com.cursojava.workshop.entities.Product;
 import com.cursojava.workshop.entities.User;
 import com.cursojava.workshop.entities.enums.OrderStatus;
 import com.cursojava.workshop.repositories.CategoryRepository;
+import com.cursojava.workshop.repositories.OrderItemRepository;
 import com.cursojava.workshop.repositories.OrderRepository;
 import com.cursojava.workshop.repositories.ProductRepository;
 import com.cursojava.workshop.repositories.UserRepository;
@@ -30,6 +32,8 @@ public class TestConfig implements CommandLineRunner{
 	private CategoryRepository categoryRepository;
 	@Autowired
 	private ProductRepository productRepository;
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -63,6 +67,14 @@ public class TestConfig implements CommandLineRunner{
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
 		
 	}
 	
---
+++ src/main/java/com/cursojava/workshop/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.cursojava.workshop.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cursojava.workshop.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}
