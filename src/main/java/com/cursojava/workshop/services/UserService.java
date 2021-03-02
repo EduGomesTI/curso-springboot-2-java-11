4136bc79d9d6e111d17970c08b6323c0f658d05b
EduGomeTI <edugomesti@outlook.com>
2021-03-02 10:53:30 -0300

    Exception handling - update

--- src/main/java/com/cursojava/workshop/services/UserService.java
+++ src/main/java/com/cursojava/workshop/services/UserService.java
@@ -3,6 +3,8 @@ package com.cursojava.workshop.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -43,9 +45,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getOne(id);
 		upateData(entity, obj);
 		return repository.save(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void upateData(User entity, User obj) {
